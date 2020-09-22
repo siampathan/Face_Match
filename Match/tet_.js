f57dc5305c854997b7c753d5eea25b1b51289ab6
siampathan005@gmail.com <siampathan005@gmail.com>
2020-09-22 22:09:53 +0600

    face-match

---
+++ Match/tet_.js
@@ -0,0 +1,389 @@
+const input = document.getElementById('input');
+const male = document.getElementById('male');
+const female = document.getElementById('female');
+const button = document.getElementById('button');
+const name_Show = document.getElementById('name_Show');
+const show_1 = document.getElementById('show_1');
+const show_2 = document.getElementById('show_2');
+const show_3 = document.getElementById('show_3');
+const photo_1 = document.getElementById('photo_1');
+const photo_2 = document.getElementById('photo_2');
+//photo show_Male
+function hri_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_1.jpg';
+    photo_1.append(photo);
+}
+function tiger_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_2.jpg';
+    photo_1.append(photo);
+}
+function chris_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_3.jpg';
+    photo_1.append(photo);
+}
+function henry_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_4.png';
+    photo_1.append(photo);
+}
+function jeet_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_5.jpg';
+    photo_1.append(photo);
+}
+function tom_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_6.jpg';
+    photo_1.append(photo);
+}
+function amir_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_7.jpg';
+    photo_1.append(photo);
+}
+function john_(){  
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_8.jpeg';
+    photo_1.append(photo);
+}
+function chri_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_9.webp';
+    photo_1.append(photo);
+}
+function jr_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_10.jpg';
+    photo_1.append(photo);
+}
+function salman_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_11.jpg';
+    photo_1.append(photo);
+}
+function girish_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_12.png';
+    photo_1.append(photo);
+}
+function sylvester_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_13.webp';
+    photo_1.append(photo);
+}
+function akshay_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_14.jpg';
+    photo_1.append(photo);
+}
+function vidyut_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_15.webp';
+    photo_1.append(photo);
+}
+function abm_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_16.jpg';
+    photo_1.append(photo);
+}
+function rock_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/male/im_17.png';
+    photo_1.append(photo);
+}
+//photo show_Female
+function shruti_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_7.jpg';
+    photo_1.append(photo);
+}
+function katrina_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_3.jpg';
+    photo_1.append(photo);
+}
+function pritee_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_4.jpg';
+    photo_1.append(photo);
+}
+function madhu_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_1.jpg';
+    photo_1.append(photo);
+}
+function amish_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_2.jpg';
+    photo_1.append(photo);
+}
+function mahima_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_5.jpg';
+    photo_1.append(photo);
+}
+function amy_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_6.jpg';
+    photo_1.append(photo);
+}
+function rashi_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_8.webp';
+    photo_1.append(photo);
+}
+function srabonti_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_9.jpg';
+    photo_1.append(photo);
+}
+function koel_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_10.jpg';
+    photo_1.append(photo);
+}
+function amber_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_11.webp';
+    photo_1.append(photo);
+}
+function bhumika_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_12.jpg';
+    photo_1.append(photo);
+}
+function payal_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_13.jpg';
+    photo_1.append(photo);
+}
+function kajal_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_14.jpg';
+    photo_1.append(photo);
+}
+function kriti_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_15.webp';
+    photo_1.append(photo);
+}
+function emma_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_16.jpg'
+    photo_1.append(photo);
+}
+function anne_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_17.jpg'
+    photo_1.append(photo);
+}
+function kath_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_18.jpg'
+    photo_1.append(photo);
+}
+function kiara_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_19.jpg'
+    photo_1.append(photo);
+}
+function  rakul_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_20.jpg'
+    photo_1.append(photo);
+}
+function paltrow_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_21.jpg'
+    photo_1.append(photo);
+}
+function ass_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_22.jpg'
+    photo_1.append(photo);
+}
+function disha_(){
+    var photo = document.createElement('img');
+    photo.src = 'img/female/im_23.jpg'
+    photo_1.append(photo);
+}
+//random_male
+function random_Male(){
+    var name = ['Hrithik Roshan','Tiger Shroff','Chris Evans','John Abraham','Chris Hemsworth','Sylvester Stallone','Akshay Kumar','ABM Sumon',
+               'Henry Cavill','Jeet','Tom Cruise','Aamir Khan','Robert Downey Jr.','Salman Khan','Girish Kumar','Vidyut Jamwal','Dwayne Johnson'];
+    var random = name[Math.floor(Math.random() * name.length)];
+    if(random == 'Hrithik Roshan'){
+        photo_2.style.backgroundImage = hri_();
+    }
+    else if(random == 'Tiger Shroff'){
+        photo_2.style.backgroundImage = tiger_();
+    }
+    else if(random == 'Chris Evans'){
+        photo_2.style.backgroundImage = chris_();
+    }
+    else if(random == 'Henry Cavill'){
+        photo_2.style.backgroundImage = henry_();
+    }
+    else if(random == 'Jeet'){
+        photo_2.style.backgroundImage = jeet_();
+    }
+    else if(random == 'Tom Cruise'){
+        photo_2.style.backgroundImage = tom_();
+    }
+    else if(random == 'Aamir Khan'){
+        photo_2.style.backgroundImage = amir_();
+    }
+    else if(random == 'John Abraham'){
+        photo_2.style.backgroundImage = john_();
+    }
+    else if(random == 'Chris Hemsworth'){
+        photo_2.style.backgroundImage = chri_();
+    }
+    else if(random == 'Robert Downey Jr.'){
+        photo_2.style.backgroundImage = jr_();
+    }
+    else if(random == 'Salman Khan'){
+        photo_2.style.backgroundImage = salman_();
+    }
+    else if(random == 'Girish Kumar'){
+        photo_2.style.backgroundImage = girish_();
+    }
+    else if(random == 'Sylvester Stallone'){
+        photo_2.style.backgroundImage = sylvester_();
+    }
+    else if(random == 'Akshay Kumar'){
+        photo_2.style.backgroundImage = akshay_();
+    }
+    else if(random == 'Vidyut Jamwal'){
+        photo_2.style.backgroundImage = vidyut_();
+    }
+    else if(random == 'ABM Sumon'){
+        photo_2.style.backgroundImage = abm_();
+    }
+    else if(random == 'Dwayne Johnson'){
+        photo_2.style.backgroundImage = rock_();
+    }
+    else{
+        photo_2.style.backgroundImage = '';
+    }
+    return random;
+}
+//random_female
+function random_female(){
+    var name = ['Shruti Hassan','Katrina Kaif','Preity Zinta','Madhubala','Amisha Patel','Mahima Nambiar','Amy Adams','Raashi Khanna','Katherine Langford','Kiara Advani','Rakul Preet',
+               'Srabanti Chatterjee','Koel Mallick','Amber Heard','Bhumika Chawla','Payal Rajput','Kajal Aggarwal','Kriti Sanon','Emma Stone','Anne Hathaway','Gwyneth Paltrow','Aishwarya Rai','Disha Patani'
+               ];
+    var random = name[Math.floor(Math.random() * name.length)];
+    if(random == 'Shruti Hassan'){
+        photo_2.style.backgroundImage = shruti_();
+    }
+    else if(random == 'Katrina Kaif'){
+        photo_2.style.backgroundImage = katrina_();
+    }
+    else if(random == 'Preity Zinta'){
+        photo_2.style.backgroundImage = pritee_();
+    }
+    else if(random == 'Madhubala'){
+        photo_2.style.backgroundImage = madhu_();
+    }
+    else if(random == 'Amisha Patel'){
+        photo_2.style.backgroundImage = amish_();
+    }
+    else if(random == 'Mahima Nambiar'){
+        photo_2.style.backgroundImage = mahima_();
+    }
+    else if(random == 'Amy Adams'){
+        photo_2.style.backgroundImage = amy_();
+    }
+    else if(random == 'Raashi Khanna'){
+        photo_2.style.backgroundImage = rashi_();
+    }
+    else if(random == 'Srabanti Chatterjee'){
+        photo_2.style.backgroundImage = srabonti_();
+    }
+    else if(random == 'Koel Mallick'){
+        photo_2.style.backgroundImage = koel_();
+    }
+    else if(random == 'Amber Heard'){
+        photo_2.style.backgroundImage = amber_();
+    }
+    else if(random == 'Bhumika Chawla'){
+        photo_2.style.backgroundImage = bhumika_();
+    }
+    else if(random == 'Payal Rajput'){
+        photo_2.style.backgroundImage = payal_();
+    }
+    else if(random == 'Kajal Aggarwal'){
+        photo_2.style.backgroundImage = kajal_();
+    }
+    else if(random == 'Kriti Sanon'){
+        photo_2.style.backgroundImage = kriti_();
+    }
+    else if(random == 'Emma Stone'){
+        photo_2.style.backgroundImage = emma_();
+    }
+    else if(random == 'Anne Hathaway'){
+        photo_2.style.backgroundImage = anne_();
+    }
+    else if(random == 'Katherine Langford'){
+        photo_2.style.backgroundImage = kath_();
+    }
+    else if(random == 'Kiara Advani'){
+        photo_2.style.backgroundImage = kiara_();
+    }
+    else if(random == 'Rakul Preet'){
+        photo_2.style.backgroundImage = rakul_();
+    }
+    else if(random == 'Gwyneth Paltrow'){
+        photo_2.style.backgroundImage = paltrow_();
+    }
+    else if(random == 'Aishwarya Rai'){
+        photo_2.style.backgroundImage = ass_();
+    }
+    else if(random == 'Disha Patani'){
+        photo_2.style.backgroundImage = disha_();
+    }
+    else{
+        photo_2.style.backgroundImage = '';
+    }
+    return random;
+}
+//gendar
+function gendar(){
+    if(male.checked == true){
+        show_2.innerHTML = 'You are Look like <br>'+ random_Male();
+        male.checked = false;
+    }
+    else if(female.checked == true){
+        show_2.innerHTML = 'You are Look like <br>'+ random_female();      
+        female.checked = false;
+    }
+    else{
+        show_2.innerText = 'Select one';
+    }
+}
+//Full Show part_
+button.addEventListener('click', () => {
+    name_Show.innerHTML = input.value;
+    if(input.value == ''){
+        name_Show.innerHTML = 'Enter your name';
+    }
+    else{
+        name_Show.innerHTML = input.value;
+        input.value = '';
+        show_1.innerHTML.display = gendar();
+        
+    }
+});
+//Upload photo part
+function showPreview(event){
+    if(event.target.files.length > 0){
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = document.getElementById("file-ip-1-preview");
+      preview.src = src;
+      preview.style.display = "block";
+    }
+  }
